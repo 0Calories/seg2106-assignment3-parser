e8bc549c96d0cb843ed966aca450492fdcbd958a
0Calories <cppanand@gmail.com>
2018-03-20 22:48:00 -0400

    Completed skeleton of Parser class

---
+++ src/Parser.java
@@ -0,0 +1,40 @@
+
+public class Parser {
+	
+	private static final int OK = 1;
+	private static final int ERROR = 0;
+	
+	private static String token;
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	private int program() {
+		return 0;
+	}
+	
+	private int statement_list() {
+		return 0;
+	}
+	
+	private int statement_list_prime() {
+		return 0;
+	}
+	
+	private int statement() {
+		return 0;
+	}
+	
+	private int expression() {
+		return 0;
+	}
+	
+	private int expression_prime() {
+		return 0;
+	}
+	
+	private int factor() {
+		return 0;
+	}
+}
